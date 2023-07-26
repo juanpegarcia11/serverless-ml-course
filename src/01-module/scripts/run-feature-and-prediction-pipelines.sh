235ce803f9a689d85dbb3857afc920b612e17949
juanpegarcia11 <120426744+juanpegarcia11@users.noreply.github.com>
2023-07-26 16:27:06 +0200

    Update run-feature-and-prediction-pipelines.sh

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -5,7 +5,8 @@ set -e
 cd src/01-module
 
 conda install ipykernel
-conda install python3
+python3 -m pip install ipykernel
+python3 -m ipykernel install --user
 
 jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
 jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
