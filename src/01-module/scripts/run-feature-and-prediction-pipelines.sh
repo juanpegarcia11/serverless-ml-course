0a1b5b399f7661dfeda81c292df41f4cb62ea029
juanpegarcia11 <120426744+juanpegarcia11@users.noreply.github.com>
2023-07-26 15:55:25 +0200

    Update run-feature-and-prediction-pipelines.sh

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -4,7 +4,7 @@ set -e
 
 cd src/01-module
 
-jupyter kernelspec list
+jupyter enable python3
 
 jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
 jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
